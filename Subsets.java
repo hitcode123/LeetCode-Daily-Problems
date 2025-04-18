08dc2505f394ea46a2a9319ee78d5c6a3bb411ac
hitcode123 <m7870899267@gmail.com>
2025-04-18 22:19:44 +0530

    recent commit for questions

--- Subsets.java
+++ Subsets.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+class SolutionSubsets{
+    Set<List<Integer>> ans;
+    List<Integer> temp;
+    public void pickNotPick(int[] nums,int i){
+        if(i==nums.length){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        temp.add(nums[i]);
+        pickNotPick(nums,i+1);
+        temp.remove(temp.size()-1);
+        pickNotPick(nums,i+1);
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        ans=new HashSet<>();
+        temp=new ArrayList<Integer>();
+        pickNotPick(nums,0);
+        List<List<Integer>> real=new ArrayList<>();
+
+        for(List<Integer> l:ans){
+            real.add(l);
+
+        }
+        return real;
+
+
+
+    }
+}
 public class Subsets {
+    public static void main(String[] args){
+
+        int[] arr=new int[]{1,2,3};
+        SolutionSubsets solutionSubsets=new SolutionSubsets();
+        System.out.println(solutionSubsets.subsets(arr));
+
+    }
 }
--- leetCodeToughQuestions/LongestNoNRepeatingSubString.java
+++ leetCodeToughQuestions/LongestNoNRepeatingSubString.java
@@ -1,4 +1,47 @@
 package leetCodeToughQuestions;
+import java.util.*;
+
+
+class Solution {
+    int lengthOfLongestSubstring(String s) {
+
+        if(s.length()==0){
+            return s.length();
+        }
+        Map<Character, Integer> charMap = new HashMap<>();
+        int low=0;
+        int high=0;
+        int calMaxLength= Integer.MIN_VALUE;
+        for(int i=0;i<s.length();i++){
+            if(charMap.containsKey(s.charAt(i))){
+
+                while(s.charAt(i)!=s.charAt(low)){
+                    charMap.put(s.charAt(low),0);
+                    low++;
+                }
+                charMap.put(s.charAt(low),0);
+                low++;
+            }
+            charMap.put(s.charAt(i),1);
+            high=i;
+            calMaxLength=Math.max(high-low+1,calMaxLength);
+
+        }
+        if(calMaxLength== Integer.MIN_VALUE){
+            return 0;
+        }
+        return calMaxLength;
+
+    }
+}
+
+
 
 public class LongestNoNRepeatingSubString {
+    public static void main(String[] args) {
+
+        Solution out=new Solution();
+        System.out.println(out.lengthOfLongestSubstring("tmmzuxt"));
+
+    }
 }
