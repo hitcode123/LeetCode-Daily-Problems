c7926af6607726b2b5349900546cac176b721f1a
hitcode123 <m7870899267@gmail.com>
2025-08-26 22:35:41 +0530

    xorSecond Way

--- A2ZSheet/XorCountK.java
+++ A2ZSheet/XorCountK.java
@@ -1,5 +1,8 @@
 package A2ZSheet;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class XorCountK {
 
     public int countXorSubSequence(int[] arr,int k){
@@ -7,22 +10,40 @@ public class XorCountK {
         int n=arr.length;
         int xorSum=0;
 
-        for(int i=0;i<n;i++){
-            int curXor=0;
-            for(int j=i;j<n;j++){
-                curXor^=arr[j];
-                if(curXor==k){
-                    xorSum++;
-                }
-            }
-        }
+//        for(int i=0;i<n;i++){
+//            int curXor=0;
+//            for(int j=i;j<n;j++){
+//                curXor^=arr[j];
+//                if(curXor==k){
+//                    xorSum++;
+//                }
+//            }
+//        }
+
+        //second way
+        Map<Integer,Integer> mp=new HashMap<>();
+        mp.putIfAbsent(0,1);
+        int xorCurrent=0;
+         for(int i=0;i<n;i++){
+             xorCurrent^=arr[i];
+             if(mp.containsKey(xorCurrent^k)){
+                 xorSum+=mp.get(xorCurrent^k);
+             }
+
+             if(mp.containsKey(xorCurrent)){
+                 mp.put(xorCurrent,mp.getOrDefault(xorCurrent,0)+1);
+
+             }else{
+                 mp.putIfAbsent(xorCurrent,1);
+             }
+         }
 
         return xorSum;
     }
     public static void main(String[] args){
         XorCountK xorCountK=new XorCountK();
         int ans=xorCountK.countXorSubSequence(new int[]{
-                5, 6, 7, 8, 9        },5);
+                4, 2, 2, 6, 4       },6);
         System.out.println(ans);
     }
 }
