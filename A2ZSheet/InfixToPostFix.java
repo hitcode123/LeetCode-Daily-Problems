3eabcaffeb91974710c6ce2ae02ab637b7a9b997
mohit <m787089927@gmail.com>
2026-03-22 01:09:05 +0530

    new commit for today

---
+++ A2ZSheet/InfixToPostFix.java
@@ -0,0 +1,56 @@
+package A2ZSheet;
+
+import java.util.Stack;
+
+public class InfixToPostFix {
+
+    public int precedence(char x){
+        if(x=='^'){
+            return 5;
+        }
+        else if(x=='*'||x=='/') {
+            return 4;
+        }else if(x=='+'||x=='-'){
+            return 3;
+        }
+        return -1;
+    }
+
+    public String ans(String ans,String real){
+        int n=real.length();
+        Stack<Character> st=new Stack<>();
+        StringBuilder ansBuilder = new StringBuilder(ans);
+        for(int i = 0; i<n; i++){
+            char present=real.charAt(i);
+            if(Character.isLetterOrDigit((present))){
+                ansBuilder.append(present);
+            } else if(present==')'){
+                while(st.peek()!='('){
+                    ansBuilder.append(st.peek());
+                    st.pop();
+                }
+                st.pop();
+            }else if(present=='('){
+                st.add('(');
+            }else{
+                while(!st.isEmpty()&&precedence(present)<precedence(st.peek())){
+                    ansBuilder.append(st.peek());
+                    st.pop();
+                }
+                st.add(present);
+            }
+
+
+        }
+        while (!st.isEmpty()) {
+            ansBuilder.append(st.pop());
+        }
+        ans = ansBuilder.toString();
+        return ans;
+    }
+    public static void main(String[] args){
+        String infix="a+b*(c^d-e)^(f+g*h)-i";
+        InfixToPostFix objResult=new InfixToPostFix();
+        System.out.println(objResult.ans("",infix));
+    }
+}
---
+++ A2ZSheet/PrefixToInfix.java
@@ -0,0 +1,28 @@
+package A2ZSheet;
+
+import java.util.Stack;
+
+public class PrefixToInfix {
+    public String convertPrefix(String test){
+        String ans= "";
+        int n=test.length();
+        Stack<String> st=new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            char current=test.charAt(i);
+            if(Character.isLetterOrDigit(current)){
+                st.add(String.valueOf(current));
+            }else{
+                String c1=st.pop();
+                String c2=st.pop();
+                st.add("("+c1+current+c2+")");
+            }
+        }
+        return st.peek();
+    }
+    public static  void main(String[] args){
+        String s="+A*BC";
+        PrefixToInfix obj=new PrefixToInfix();
+        String ans=obj.convertPrefix(s);
+        System.out.println(ans);
+    }
+}
