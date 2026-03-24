d576dcba70b5fdf95c9eb6a8dcd1ecd2c0639298
mohit <m787089927@gmail.com>
2026-03-24 21:56:37 +0530

    PostFixToInfix

---
+++ A2ZSheet/PostFixToInfix.java
@@ -0,0 +1,32 @@
+package A2ZSheet;
+
+import java.util.Stack;
+
+public class PostFixToInfix {
+    public static void main(String[] args){
+        String exp="ab+c*";
+        String ans=findSolutionForPostFixToInfix(exp);
+
+        System.out.println("hence the output for the given question is"+ans.toString());
+    }
+
+    private static String findSolutionForPostFixToInfix(String exp) {
+        int n=exp.length();
+        Stack<String> st=new Stack<>();
+
+        for(int i=0;i<n;i++){
+            char present=exp.charAt(i);
+            if(Character.isLetterOrDigit(present)){
+                st.add(String.valueOf(present));
+            }else {
+                String first = st.pop();
+                String second = st.pop();
+                st.add("(" + second + String.valueOf(present) + first + ")");
+            }
+        }
+
+
+
+        return st.peek();
+    }
+}
